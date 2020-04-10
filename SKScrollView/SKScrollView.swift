a09832cd1f6d0ab66140e83e7a49c7a04ae76a60
Tim :D <timothy5216@me.com>
2020-04-10 15:32:37 -0700

    wip scroll view for SpriteKit's SKView

---
+++ SKScrollView/SKScrollView.swift
@@ -0,0 +1,153 @@
+//
+//  SKScrollView.swift
+//  SKScrollView
+//
+//  Created by Tim Desir on 4/9/20.
+//  Copyright © 2020 Lunar2 Inc. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class SKScrollView: UIView {
+
+    // MARK: - Variables & Initialization
+	
+	private(set) var skView: SKView!
+	private(set) var scrollView: UIScrollView!
+	
+	private var cameraNode: SKCameraNode!
+	
+	private(set) weak var scene: SKScene? = nil
+	private(set) var contentSize: CGSize = .zero
+	
+	private var overlayView: UIView!
+	
+	// --
+	
+	init(frame: CGRect, contentSize: CGSize) {
+		super.init(frame: frame)
+		self.contentSize = contentSize
+		self.backgroundColor = UIColor(white: 0.1, alpha: 1.0)
+		
+		// init scroll view
+		scrollView = UIScrollView(frame: .zero)
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		//scrollView.contentInset = UIEdgeInsets(top: 500, left: 500, bottom: 0, right: 0)
+		scrollView.contentSize = CGSize(width: contentSize.width, height: contentSize.height)
+		scrollView.clipsToBounds = true
+		scrollView.maximumZoomScale = 10.0
+		scrollView.minimumZoomScale = 1.0
+		//scrollView.showsVerticalScrollIndicator = false
+		//scrollView.showsHorizontalScrollIndicator = false
+		addSubview(scrollView)
+		NSLayoutConstraint.activate ([
+			scrollView.topAnchor.constraint(equalTo: topAnchor),
+			scrollView.leftAnchor.constraint(equalTo: leftAnchor),
+			scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+			scrollView.rightAnchor.constraint(equalTo: rightAnchor)
+		])
+		
+		// init overlay view
+		/*overlayView = UIView(frame: .zero)
+		overlayView.translatesAutoresizingMaskIntoConstraints = false
+		scrollView.addSubview(overlayView)
+		NSLayoutConstraint.activate ([
+			overlayView.widthAnchor.constraint(equalToConstant: contentSize.width),
+			overlayView.heightAnchor.constraint(equalToConstant: contentSize.height)
+		])*/
+		
+		// init sk view
+		skView = SKView(frame: .zero)
+		skView.translatesAutoresizingMaskIntoConstraints = false
+		skView.showsPhysics = true
+		scrollView.addSubview(skView)
+		NSLayoutConstraint.activate ([
+			skView.topAnchor.constraint(equalTo: topAnchor),
+			skView.leftAnchor.constraint(equalTo: leftAnchor),
+			skView.bottomAnchor.constraint(equalTo: bottomAnchor),
+			skView.rightAnchor.constraint(equalTo: rightAnchor)
+		])
+		
+		// init scene
+		let scene: SKScene? = SKScene(fileNamed: "MyScene")
+		scene?.scaleMode = .resizeFill
+		skView.presentScene(scene)
+		self.scene = scene
+		
+		// init camera node
+		cameraNode = SKCameraNode()
+		self.scene?.camera = cameraNode
+
+		//
+		let hdif = (contentSize.height - scrollView.bounds.size.height) / 2
+		let vdif = (contentSize.width - scrollView.bounds.size.width) / 2
+		self.cameraNode.position = CGPoint(x: 0 - vdif, y: (0 - hdif) * -1)
+		
+		//
+		let centerOffsetX = (scrollView.contentSize.width - scrollView.frame.size.width) / 2
+		let centerOffsetY = (scrollView.contentSize.height - scrollView.frame.size.height) / 2
+		let centerPoint = CGPoint(x: centerOffsetX, y: centerOffsetY)
+		scrollView.setContentOffset(centerPoint, animated: false)
+		
+		//
+		scrollView.delegate = self
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Layout
+	
+	override func layoutSubviews() {
+//		let contentOffset = scrollView.contentOffset
+//		let hdif = (contentSize.height/2)-(scrollView.bounds.size.height/2)
+//		let vdif = (contentSize.width/2)-(scrollView.bounds.size.width/2)
+//
+//		self.cameraNode.position = CGPoint(x: contentOffset.x - vdif, y: (contentOffset.y - hdif) * -1)
+		
+		//cameraNode.position = CGPoint(x: cameraNode.position.x+vdif, y: cameraNode.position.y+hdif)
+		//skView.frame = CGRect(x: (contentSize.width/2)-con, y: 0, width: contentSize.width, height: contentSize.height)
+		//skView.center = scrollView.center
+		//scrollView.contentInset = UIEdgeInsets(top: bounds.size.height-contentSize.height, left: bounds.size.width-contentSize.width, bottom: 0, right: 0)
+	}
+	
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension SKScrollView: UIScrollViewDelegate {
+	
+	// MARK: Scrolling
+	
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		let contentOffset = scrollView.contentOffset
+		let hdif = (contentSize.height/2)-(scrollView.bounds.size.height/2)
+		let vdif = (contentSize.width/2)-(scrollView.bounds.size.width/2)
+		
+		self.cameraNode.position = CGPoint(x: contentOffset.x - vdif, y: (contentOffset.y - hdif) * -1)
+		
+		
+//		let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0)
+//		let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0)
+//		scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
+	}
+	
+	// MARK: Zooming
+	
+	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+		return nil
+	}
+	
+	func scrollViewDidZoom(_ scrollView: UIScrollView) {
+//		let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0)
+//		let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0)
+//		scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
+	}
+	
+	func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+		//self.cameraNode.setScale(0.1)
+	}
+	
+}
--- SKScrollView/ViewController.swift
+++ SKScrollView/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
+	
+	private var skScrollView: SKScrollView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		// init scroll view
+		let sceneSize: CGSize = CGSize(width: 1920, height: 1080)
+		skScrollView = SKScrollView(frame: .zero, contentSize: sceneSize)
+		skScrollView.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(skScrollView)
+		NSLayoutConstraint.activate ([
+			skScrollView.topAnchor.constraint(equalTo: view.topAnchor),
+			skScrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+			skScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			skScrollView.rightAnchor.constraint(equalTo: view.rightAnchor)
+		])
+		
+		// add label
+		let hscrollingLabel = SKLabelNode(fontNamed: "HelveticaNeue")
+		hscrollingLabel.name = "title"
+		hscrollingLabel.text = "SKScrollView"
+		hscrollingLabel.fontSize = 70
+		hscrollingLabel.fontColor = SKColor.white
+		hscrollingLabel.position = .zero
+		hscrollingLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.center
+		skScrollView.scene?.addChild(hscrollingLabel)
 	}
 
 
